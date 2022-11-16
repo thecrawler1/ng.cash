e0a005267ff9ee1944760e0f8df52de32e27e686
Gabriel Alves <gabrielalves@outlook.com>
2022-11-16 17:37:19 -0300

    tests: create unit tests for the account entity

---
+++ backend/tests/unit/entities/value-objects/monetaryValue.spec.ts
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import InvalidMonetaryValueTypeError from '../../../../src/entities/errors/InvalidMonetaryValueTypeError';
+import InvalidNegativeMonetaryValueError from '../../../../src/entities/errors/InvalidNegativeMonetaryValueError';
+import MonetaryValue from '../../../../src/entities/value-objects/MonetaryValue';
+
+describe('MonetaryValue value object', function () {
+  it('should create a MonetaryValue', function () {
+    const id = MonetaryValue.create(5);
+
+    expect(id.value).to.be.equal(5);
+  });
+
+  it('should throw an error when the type is not number', function () {
+    const createMonetaryValue = () => MonetaryValue.create('5' as any);
+
+    expect(createMonetaryValue).to.throw(InvalidMonetaryValueTypeError).and.contain({
+      messageCode: 'INVALID_MONETARY_VALUE_TYPE',
+    });
+  });
+
+  it('should throw an error when the value is negative', function () {
+    const createMonetaryValue = () => MonetaryValue.create(-5);
+
+    expect(createMonetaryValue).to.throw(InvalidNegativeMonetaryValueError).and.contain({
+      messageCode: 'INVALID_NEGATIVE_MONETARY_VALUE',
+    });
+  });
+});
