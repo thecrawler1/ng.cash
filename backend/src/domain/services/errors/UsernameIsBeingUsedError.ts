143ce9b3ae12fa90891ce4ed489cfc42f0da11ac
Gabriel Alves <gabrielalves@outlook.com>
2022-11-17 19:29:15 -0300

    fix: remove error name

--- backend/src/domain/services/errors/UsernameIsBeingUsedError.ts
+++ backend/src/domain/services/errors/UsernameIsBeingUsedError.ts
@@ -3,7 +3,5 @@ import BadRequestError from '@domain/errors/BadRequestError';
 export default class UsernameIsBeingUsedError extends BadRequestError {
   constructor() {
     super('The username is already being used', 'USERNAME_IS_BEING_USED');
-
-    this.name = 'UsernameIsBeingUsedError';
   }
 }
