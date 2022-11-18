a4ab8f05da61da7e2de52d94a3b76bd6ad8c0d98
Gabriel Alves <gabrielalves@outlook.com>
2022-11-18 14:52:20 -0300

    feat: add the get user transactions controller

---
+++ backend/src/domain/controllers/GetUserTransactionsController.ts
@@ -0,0 +1,30 @@
+import TransactionDTO from '@domain/entities/dtos/TransactionDTO';
+import Transaction from '@domain/entities/Transaction';
+import User from '@domain/entities/User';
+import IController from '@domain/interfaces/controller/IController';
+import IRequest from '@domain/interfaces/controller/IRequest';
+import IResponse from '@domain/interfaces/controller/IResponse';
+import IGetUserTransactionsService, { Filters, TransactionType } from '@domain/interfaces/services/IGetUserTransactionsService';
+import { ok } from './helpers/httpResponses';
+
+export default class GetUserTransactionsController implements IController {
+  constructor(private getUserTransactionsService: IGetUserTransactionsService) {}
+
+  async handle(request: IRequest): Promise<IResponse> {
+    const user: User = User.create(request.payload.userDTO);
+    const filters: Filters = GetUserTransactionsController.createFilters(request);
+    const transactions: Transaction[] = await this.getUserTransactionsService.perform(user, filters);
+    const dtos: TransactionDTO[] = transactions.map((transaction) => transaction.toDTO());
+
+    return ok(dtos);
+  }
+
+  private static createFilters({ query }: IRequest): Filters {
+    const date = query.date ? new Date(query.date) : undefined;
+    const transactionType = query.transactionType in TransactionType
+      ? query.transactionType as TransactionType
+      : TransactionType.both;
+
+    return { date, transactionType };
+  }
+}
