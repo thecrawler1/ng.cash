937550102e36a773006cb66f752826fbc096a089
Gabriel Alves <gabrielalves@outlook.com>
2022-11-18 13:16:01 -0300

    feat: add the login controller

--- backend/src/domain/controllers/helpers/httpResponses.ts
+++ backend/src/domain/controllers/helpers/httpResponses.ts
@@ -1,5 +1,7 @@
 import IResponse from '@domain/interfaces/controller/IResponse';
 
 const STATUS_CODE_CREATED = 201;
+const STATUS_CODE_OK = 200;
 
 export const created = (data: any) => <IResponse> { data, statusCode: STATUS_CODE_CREATED };
+export const ok = (data: any) => <IResponse> { data, statusCode: STATUS_CODE_OK };
