aec43e77e1805ead97f86d35b8e1364c20b4391e
Gabriel Alves <gabrielalves@outlook.com>
2022-11-20 15:49:07 -0300

    feat: create the middleware adapter

---
+++ backend/src/infrastructure/adapters/express/MiddlewareAdapter.ts
@@ -0,0 +1,17 @@
+import { RequestHandler, Request, Response, NextFunction } from 'express';
+import IMiddleware from '@domain/interfaces/middleware/IMiddleware';
+import IRequest from '@domain/interfaces/middleware/IRequest';
+import IResponse from '@domain/interfaces/middleware/IResponse';
+
+export default class MiddlewareAdapter {
+  static adapt(middleware: IMiddleware): RequestHandler {
+    return (req: Request, _res: Response, next: NextFunction) => {
+      const request: IRequest = { token: req.headers.authorization };
+      const response: IResponse = middleware.handle(request);
+
+      req.body = response.body;
+
+      next();
+    };
+  }
+}
