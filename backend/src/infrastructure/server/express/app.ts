2c59ac8b67536361f5a6d6c6356cc81b256b7705
Gabriel Alves <gabrielalves@outlook.com>
2022-11-20 16:03:37 -0300

    feat: create app getter

--- backend/src/infrastructure/server/express/app.ts
+++ backend/src/infrastructure/server/express/app.ts
@@ -2,13 +2,17 @@ import express, { Express } from 'express';
 import 'express-async-errors';
 
 export default class App {
-  private readonly app: Express;
+  private readonly _app: Express;
 
   constructor() {
-    this.app = express();
+    this._app = express();
   }
 
   start(port: number | string): void {
     this.app.listen(port, () => console.log(`Running on port ${port}`));
+  }
+
+  get app(): Express {
+    return this._app;
   }
 }
