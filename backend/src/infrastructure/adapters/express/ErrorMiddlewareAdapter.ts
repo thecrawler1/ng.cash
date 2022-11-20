c818573fab754c1a17f1d860a58edac06c6ad884
Gabriel Alves <gabrielalves@outlook.com>
2022-11-20 18:09:57 -0300

    style: remove unnecessary parameter

--- backend/src/infrastructure/adapters/express/ErrorMiddlewareAdapter.ts
+++ backend/src/infrastructure/adapters/express/ErrorMiddlewareAdapter.ts
@@ -1,10 +1,10 @@
-import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import { Request, Response, ErrorRequestHandler } from 'express';
 import IErrorMiddleware from '@domain/interfaces/middleware/IErrorMiddleware';
 import IErrorResponse from '@domain/interfaces/middleware/IErrorResponse';
 
 export default class ErrorMiddlewareAdapter {
   static adapt(errorMiddleware: IErrorMiddleware): ErrorRequestHandler {
-    return (error: Error, _req: Request, res: Response, _next: NextFunction) => {
+    return (error: Error, _req: Request, res: Response) => {
       const { statusCode, message, messageCode }: IErrorResponse = errorMiddleware.handle(error);
 
       res.status(statusCode).json({ message, messageCode });
