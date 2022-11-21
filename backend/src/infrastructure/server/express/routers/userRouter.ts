22b0b1b035a78bbd98acbcf5aed77e9297942383
Gabriel Alves <gabrielalves@outlook.com>
2022-11-21 14:49:57 -0300

    feat: add the /users/balance get endpoint

--- backend/src/infrastructure/server/express/routers/userRouter.ts
+++ backend/src/infrastructure/server/express/routers/userRouter.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import CreateUserControllerFactory from '@infrastructure/factories/CreateUserControllerFactory';
 import RouteAdapter from '@infrastructure/adapters/express/RouteAdapter';
+import GetUserBalanceControllerFactory from '@infrastructure/factories/GetUserBalanceControllerFactory';
+import MiddlewareAdapter from '@infrastructure/adapters/express/MiddlewareAdapter';
+import AuthenticatorMiddlewareFactory from '@infrastructure/factories/AuthenticatorMiddlewareFactory';
 
 const createUserController = CreateUserControllerFactory.make();
+const getUserBalanceController = GetUserBalanceControllerFactory.make();
+const auth = MiddlewareAdapter.adapt(AuthenticatorMiddlewareFactory.make());
 const router = Router();
 
 router.post('/', RouteAdapter.adapt(createUserController));
+router.get('/balance', auth, RouteAdapter.adapt(getUserBalanceController));
 
 export default router;
