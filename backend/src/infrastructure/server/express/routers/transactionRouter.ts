62fe627576db406aacc8ea1614d83c03905ca0ea
Gabriel Alves <gabrielalves@outlook.com>
2022-11-21 19:01:16 -0300

    feat: add the /transactions get endpoint

--- backend/src/infrastructure/server/express/routers/transactionRouter.ts
+++ backend/src/infrastructure/server/express/routers/transactionRouter.ts
@@ -3,11 +3,14 @@ import RouteAdapter from '@infrastructure/adapters/express/RouteAdapter';
 import MiddlewareAdapter from '@infrastructure/adapters/express/MiddlewareAdapter';
 import AuthenticatorMiddlewareFactory from '@infrastructure/factories/AuthenticatorMiddlewareFactory';
 import MakeTransferControllerFactory from '@infrastructure/factories/MakeTransferControllerFactory';
+import GetUserTransactionsControllerFactory from '@infrastructure/factories/GetUserTransactionsControllerFactory';
 
 const makeTransferController = MakeTransferControllerFactory.make();
+const getUserTransactionsController = GetUserTransactionsControllerFactory.make();
 const auth = MiddlewareAdapter.adapt(AuthenticatorMiddlewareFactory.make());
 const router = Router();
 
 router.post('/', auth, RouteAdapter.adapt(makeTransferController));
+router.get('/', auth, RouteAdapter.adapt(getUserTransactionsController));
 
 export default router;
