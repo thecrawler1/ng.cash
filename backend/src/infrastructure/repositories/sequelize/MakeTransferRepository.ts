147251d83dc5a524d93daa9f4b6e14552986ac20
Gabriel Alves <gabrielalves@outlook.com>
2022-11-21 19:25:10 -0300

    refactor: make code more legible

--- backend/src/infrastructure/repositories/sequelize/MakeTransferRepository.ts
+++ backend/src/infrastructure/repositories/sequelize/MakeTransferRepository.ts
@@ -1,9 +1,10 @@
+import { Transaction as SequelizeTransaction } from 'sequelize';
+import Id from '@domain/entities/value-objects/id';
 import User from '@domain/entities/User';
 import Transaction from '@domain/entities/Transaction';
 import MonetaryValue from '@domain/entities/value-objects/monetary-value';
 import IMakeTransferRepository from '@domain/interfaces/repositories/IMakeTransferRepository';
 import UserNotFoundError from './errors/UserNotFoundError';
-import AccountNotFoundError from './errors/AccountNotFoundError';
 import UserModel from './database/models/User';
 import TransactionModel from './database/models/Transaction';
 import AccountModel from './database/models/Account';
@@ -11,40 +12,35 @@ import sequelize from './database';
 
 export default class MakeTransferRepository implements IMakeTransferRepository {
   async perform(sourceUser: User, destinationUser: User, amount: MonetaryValue): Promise<Transaction> {
-    const sourceUserModel: UserModel | null = await UserModel.findOne({
-      where: { id: sourceUser.id.value },
-      include: [AccountModel],
-    });
-    const destinationUserModel: UserModel | null = await UserModel.findOne({
-      where: { id: destinationUser.id.value },
-      include: [AccountModel],
-    });
-
-    if (!sourceUserModel || !destinationUserModel) {
-      throw new UserNotFoundError();
-    }
-
-    const sourceAccountModel: AccountModel | null = await sourceUserModel.$get('account');
-    const destinationAccountModel: AccountModel | null = await destinationUserModel.$get('account');
-
-    if (!sourceAccountModel || !destinationAccountModel) {
-      throw new AccountNotFoundError();
-    }
+    const sourceUserModel = await this.getUserModelById(sourceUser.id);
+    const destinationUserModel = await this.getUserModelById(destinationUser.id);
 
     return sequelize.transaction(async (transaction) => {
-      const newSourceBalance = sourceAccountModel.balance - amount.value;
-      await sourceAccountModel.update({ balance: newSourceBalance }, { transaction });
-
-      const newDestinationBalance = destinationAccountModel.balance + amount.value;
-      await destinationAccountModel.update({ balance: newDestinationBalance }, { transaction });
+      await this.updateAccountBalance(sourceUserModel.account, -amount.value, transaction);
+      await this.updateAccountBalance(destinationUserModel.account, amount.value, transaction);
 
       const transactionModel: TransactionModel = await TransactionModel.create({
-        debitedAccountId: sourceAccountModel.id,
-        creditedAccountId: destinationAccountModel.id,
+        debitedAccountId: sourceUserModel.accountId,
+        creditedAccountId: destinationUserModel.accountId,
         value: amount.value,
       }, { transaction });
 
       return Transaction.create(transactionModel.toJSON());
     });
+  }
+
+  private async getUserModelById(id: Id): Promise<UserModel> {
+    const userModel: UserModel | null = await UserModel.findOne({
+      where: { id: id.value },
+      include: [AccountModel],
+    });
+
+    if (!userModel) throw new UserNotFoundError();
+
+    return userModel;
+  }
+
+  private async updateAccountBalance(account: AccountModel, value: number, transaction: SequelizeTransaction) {
+    await account.update({ balance: account.balance + value }, { transaction });
   }
 }
