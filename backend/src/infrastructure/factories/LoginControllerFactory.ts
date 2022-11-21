77b2466c5d40f00213ce6d8246e98ffc02655941
Gabriel Alves <gabrielalves@outlook.com>
2022-11-21 12:27:25 -0300

    feat: create the factory to make the login controller

---
+++ backend/src/infrastructure/factories/LoginControllerFactory.ts
@@ -0,0 +1,23 @@
+import LoginController from '@domain/controllers/LoginController';
+import GetUserPasswordHashByUsernameRepository from '@infrastructure/repositories/sequelize/GetUserPasswordHashByUsernameRepository';
+import GetUserByUsernameRepository from '@infrastructure/repositories/sequelize/GetUserByUsernameRepository';
+import HashComparer from '@infrastructure/password-hash-manager/bcrypt/HashComparer';
+import UserTokenEncoder from '@infrastructure/token-manager/UserTokenEncoder';
+import LoginService from '@domain/services/LoginService';
+
+export default abstract class LoginControllerFactory {
+  static make(): LoginController {
+    const getUserPasswordHashByUsernameRepository = new GetUserPasswordHashByUsernameRepository();
+    const getUserByUsernameRepository = new GetUserByUsernameRepository();
+    const hashComparerRepository = new HashComparer();
+    const userTokenEncoderRepository = new UserTokenEncoder();
+    const loginService = new LoginService(
+      getUserPasswordHashByUsernameRepository,
+      getUserByUsernameRepository,
+      hashComparerRepository,
+      userTokenEncoderRepository,
+    );
+
+    return new LoginController(loginService);
+  }
+}
