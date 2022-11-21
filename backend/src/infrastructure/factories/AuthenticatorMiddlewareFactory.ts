5e83f11d398014b3b35b3e39abc306a720c7d05b
Gabriel Alves <gabrielalves@outlook.com>
2022-11-21 14:47:01 -0300

    feat: create the factory to make the authenticator middleware

---
+++ backend/src/infrastructure/factories/AuthenticatorMiddlewareFactory.ts
@@ -0,0 +1,10 @@
+import AuthenticatorMiddleware from '@domain/middlewares/AuthenticatorMiddleware';
+import UserTokenDecoder from '@infrastructure/token-manager/UserTokenDecoder';
+
+export default abstract class AuthenticatorMiddlewareFactory {
+  static make(): AuthenticatorMiddleware {
+    const userTokenDecoder = new UserTokenDecoder();
+
+    return new AuthenticatorMiddleware(userTokenDecoder);
+  }
+}
