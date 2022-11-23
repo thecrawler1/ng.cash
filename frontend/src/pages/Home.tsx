caaff7367b8fabc6667550a3c213298bb8f4c6fc
Gabriel Alves <gabrielalves@outlook.com>
2022-11-22 22:32:29 -0300

    feat: add username and balance in the home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import User from "../interfaces/User";
+import { getBalance, getUser } from "../services/requests";
+import Loading from "./Loading";
+
 function Home() {
-  return <h2>Home page</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<User>();
+  const [balance, setBalance] = useState(0);
+
+  const getData = async () => {
+    const user = await getUser();
+    const balance = await getBalance();
+
+    setUser(user);
+    setBalance(balance);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return isLoading
+    ? <Loading />
+    : (
+      <main>
+        <section>
+          <h1>NG.CASH</h1>
+          <section>
+            <span>Saldo</span>
+            <h1>{`R$${balance.toFixed(2).replaceAll('.', ',')}`}</h1>
+            <button>Nova transaferência</button>
+          </section>
+          <section>
+            <h3>{user?.username}</h3>
+            <button>Sair</button>
+          </section>
+        </section>
+      </main>
+    );
 }
 
 export default Home;
