271864df5a0afe71c9229f97450a39ea0b33e867
Gabriel Alves <gabrielalves@outlook.com>
2022-11-23 19:27:14 -0300

    feat: add custom error messages

--- frontend/src/utils/getUIErrorMessage.ts
+++ frontend/src/utils/getUIErrorMessage.ts
@@ -4,6 +4,9 @@ const errors: { [key: string]: string } = {
   PASSWORD_MISSING_REQUIRED_CHARACTER: 'A senha deve ter pelo menos um número e uma letra maiúscula.',
   USERNAME_IS_BEING_USED: 'Este nome de usuário já está em uso.',
   REQUIRED_PAYLOAD_NOT_PROVIDED: 'Todos os campos devem ser preenchidos.',
+  USER_NOT_FOUND: 'Usuário não encontrado.',
+  INVALID_TRANSFER_TO_YOURSELF: 'Não é possível transferir para sua própria conta.',
+  INSUFFICIENT_BALANCE: 'Saldo insuficiente.',
 };
 
 export default function getUIErrorMessage(messageCode?: string) {
